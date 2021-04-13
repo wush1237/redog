47fa1b99e6ea157ffd3646cee0bc85754bba1228
wush1237 <wush0913@gmail.cpm>
2021-04-13 16:26:13 +0800

    racing now test

--- src/main/java/com/tom/racing/racing.java
+++ src/main/java/com/tom/racing/racing.java
@@ -2,13 +2,12 @@ package com.tom.racing;
 
 public class racing {
     public static void main(String[] args) {
-        String name1 = "Hores1";
-        Horse H2 = new Horse();
-        H2.start(); //開始執行Hores
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Horse 1 " + i );
-        }
-
-
+        Hores h1 = new Hores();
+        Hores h2 = new Hores();
+        HoresRunnable h3 = new HoresRunnable();
+        Thread th = new Thread(h3);
+        th.start();
+        h1.start();
+        h2.start();
     }
 }
