e8217098d6d0440624d806e44c45530d8985a5a4
wush1237 <wush0913@gmail.cpm>
2021-04-13 15:09:39 +0800

    racing

--- src/main/java/com/tom/racing/racing.java
+++ src/main/java/com/tom/racing/racing.java
@@ -3,6 +3,12 @@ package com.tom.racing;
 public class racing {
     public static void main(String[] args) {
         String name1 = "Hores1";
+        Horse H2 = new Horse();
+        H2.start(); //開始執行Hores
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Horse 1 " + i );
+        }
+
 
     }
 }
